862025cd7733e79fa6b95d268816f3c37765eb69
Nicholas Ormrod <njormrod@fb.com>
2024-06-04 09:45:52 -0700

    fancy zsh prompt

--- configs/shell/profile.zsh
+++ configs/shell/profile.zsh
@@ -9,5 +9,5 @@ setopt HIST_IGNORE_DUPS
 # Misc
 
 # Prompt
-PROMPT=$'%(?..%F{red}%B[Exit %?]\n%b%f)%F{cyan}%~ $%f '
+PROMPT=$'%(?..%F{red}%B[Exit %?]\n%b%f)%F{cyan}%~ %F{red}\U2771%F{208}\U2771%F{green}\U2771%f '
 RPROMPT='%*'
