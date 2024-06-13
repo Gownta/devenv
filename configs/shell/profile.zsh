2da06ba5dd0981403b18e276b704d2c0856bb459
Nicholas Ormrod <nicholas.ormrod+github@gmail.com>
2024-06-13 13:28:23 -0700

    use dir collapsing in zsh command

--- configs/shell/profile.zsh
+++ configs/shell/profile.zsh
@@ -9,5 +9,6 @@ setopt HIST_IGNORE_DUPS
 # Misc
 
 # Prompt
-PROMPT=$'%(?..%F{red}%B[Exit %?]\n%b%f)%F{cyan}%~ %F{red}\U2771%F{208}\U2771%F{green}\U2771%f '
+setopt PROMPT_SUBST
+PROMPT=$'%(?..%F{red}%B[Exit %?]\n%b%f)%F{cyan}$(prompt_dir_sub) %F{red}\U2771%F{208}\U2771%F{green}\U2771%f '
 RPROMPT='%*'
