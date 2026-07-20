371fb5eb985d8cc5b0273a2da2496fa85a9b3cf0
Nicholas Ormrod <nicholas.ormrod+github@gmail.com>
2026-07-20 10:08:42 -0700

    rm old devserver

--- bin/ssh_command.py
+++ bin/ssh_command.py
@@ -138,7 +138,6 @@ Dsts = (
         ),
     ]
     + make_devserver_dsts("devvm62897.cco0", with_eternal=True)
-    + make_devserver_dsts("devvm7569.cco0", with_eternal=True)
     + [Dst("localhost")]
 )
 #host[3]="alpha-pi"
