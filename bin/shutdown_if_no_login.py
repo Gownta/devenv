9d3b5ac598f1d786b772bb9cd27de2b7b0d5065e
Nicholas Ormrod <nicholas.ormrod+github@gmail.com>
2026-09-18 21:09:40 -0700

    60 minutes grace before shutdown

--- bin/shutdown_if_no_login.py
+++ bin/shutdown_if_no_login.py
@@ -8,6 +8,8 @@ import time
 
 from datetime import datetime, timedelta
 
+LOGOUT_MINUTES_BEFORE_SHUTDOWN = 60
+
 
 def logout_length(user):
     last = subprocess.check_output(["last", "-n2", "--time-format", "iso", user], text=True).splitlines()[0]
@@ -26,7 +28,7 @@ def go():
     delta = logout_length(user)
     minutes = math.floor(delta.total_seconds()) // 60
     print(f"User {user} has been logged out for {minutes} minutes")
-    if minutes >= 20:
+    if minutes >= LOGOUT_MINUTES_BEFORE_SHUTDOWN:
         print("Shutting down")
         subprocess.run(["sudo", "shutdown", "+5", "njormrod not logged in; shutting down; `sudo shutdown -c` to cancel"])
     else:
